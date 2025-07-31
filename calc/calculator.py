4562b7d85744e60d02e6abdef32fad0f138f4fa0
Norman Mwagi <normanmwagi23@gmail.com>
2025-07-31 08:01:10 +0300

    first commit

---
+++ calc/calculator.py
@@ -0,0 +1,11 @@
+firstNo = int(input("Enter the first number: "))
+secondNo = int(input("Enter the second number: "))
+
+sum = firstNo + secondNo
+sub = firstNo - secondNo
+mul = firstNo * secondNo
+div = firstNo / secondNo
+print("sum: ", sum)
+print("sub: ", sub)
+print("mul: ", mul)
+print("div: ", div)
